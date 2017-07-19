2eb0650c197bfd753ef3b756089043c4e9cbd23d
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2017-07-19 22:38:00 +0200

    add missing file and fix makefile

---
+++ sw/ground_segment/cockpit/x11_wrapper.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Gautier Hattenberger <gautier.hattenberger@enac.fr>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** X11 wrapper
+ *
+ * used to enable X11 thread protection
+ */
+
+#include <X11/Xlib.h>
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/fail.h>
+
+CAMLprim value
+ml_XInitThreads( value unit )
+{
+  XInitThreads();
+  return Val_unit;
+}
+
